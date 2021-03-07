7c18b285e28bdfc4a8befe972ce759121c368992
KuanYu <george310909@gmail.com>
2021-03-07 13:41:41 +0800

    Fixed warning

--- src/SheetsDataBase.js
+++ src/SheetsDataBase.js
@@ -4,7 +4,7 @@ const SheetsTable = require("./SheetsTable");
 
 module.exports = {
 	"getInstance": function(credentialsPath) {
-		GoogleAuthorize.authorize(credentialsPath);
+		GoogleAuthorize.authorize(credentialsPath).catch(() => { });
 		return {
 			"Schema": SheetsSchema,
 			"Table": SheetsTable
